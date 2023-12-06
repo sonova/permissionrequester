53ab2383531f8573041b81f76957be9b2baaffeb
Carla Vaquero <carla.vaqueroverde@sonova.com>
2023-12-06 08:32:15 +0100

    adds lib dependency to kts

--- sampleapp/build.gradle.kts
+++ sampleapp/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation(project(":permissionrequester"))
 
     testImplementation("junit:junit:4.13.2")
 
