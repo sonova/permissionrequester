a48904c0c95d4283859f59e717e11068f41ae60a
Carla Vaquero <carla.vaqueroverde@sonova.com>
2023-12-06 08:34:37 +0100

    integrates permission request into sampleapp

--- sampleapp/src/main/java/com/sonova/android/permissionrequester/sample/MainActivity.kt
+++ sampleapp/src/main/java/com/sonova/android/permissionrequester/sample/MainActivity.kt
@@ -1,19 +1,28 @@
 package com.sonova.android.permissionrequester.sample
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sonova.android.permissionrequester.PermissionRequester
 import com.sonova.android.permissionrequester.sample.ui.theme.PermissionRequesterTheme
 
@@ -21,18 +30,15 @@ class MainActivity : ComponentActivity() {
 
     private lateinit var permissionRequester: PermissionRequester
     override fun onCreate(savedInstanceState: Bundle?) {
+        permissionRequester = createPermissionRequester()
         super.onCreate(savedInstanceState)
         setContent {
             PermissionRequesterTheme {
                 // A surface container using the 'background' color from the theme
-                val launcher = rememberLauncherForActivityResult(
+                rememberLauncherForActivityResult(
                     ActivityResultContracts.RequestPermission()
-                ) { isGranted ->
-                    if (isGranted) {
-                        // Open camera
-                    } else {
-                        // Show dialog
-                    }
+                ) {
+
                 }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -43,21 +49,71 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private fun createPermissionRequester() = PermissionRequester.Builder()
+        .requirePermissions(
+            {
+                titleResId = R.string.permission_rationale_title
+                messageResId = R.string.permission_rationale_description
+            },
+            {
+                titleResId = R.string.permission_settings_title
+                messageResId = R.string.permission_settings_description
+            },
+            PERMISSIONS
+        )
+        .build(this) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                revokeSelfPermissionsOnKill(PERMISSIONS)
+            }
+        }
+
+    companion object {
+        private val PERMISSIONS = listOf(
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    }
 }
 
 @Composable
 fun PermissionButton(permissionRequester: PermissionRequester) {
-    Column {
-        Button(onClick = { permissionRequester.request() }) {
-            Text("Request permission with rationale")
+    Column(modifier = Modifier.padding(24.dp)) {
+        val showPermissionDialog = remember { mutableStateOf(true) }
+        Text(
+            modifier = Modifier.padding(bottom = 16.dp),
+            text = "Request permission will show " +
+                    if (showPermissionDialog.value) "permission dialog" else "rationale"
+        )
+        Button(modifier = Modifier.align(Alignment.CenterHorizontally),
+            onClick = {
+                showPermissionDialog.value = false
+                permissionRequester.request(false)
+            }) {
+            Text("Request locations permissions")
+        }
+        Spacer(modifier = Modifier.height(36.dp))
+
+        Text(
+            modifier = Modifier.padding(bottom = 16.dp),
+            text = "Request permission without rationale"
+        )
+        Button(modifier = Modifier.align(Alignment.CenterHorizontally),
+            onClick = {
+                showPermissionDialog.value = true
+                permissionRequester.request(true)
+            }) {
+            Text("Request locations permissions")
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PermissionButtonPreview() {
     PermissionRequesterTheme {
-        PermissionButton(PermissionRequester.Builder().build(LocalContext.current as ComponentActivity))
+        PermissionButton(
+            PermissionRequester.Builder().build(LocalContext.current as ComponentActivity)
+        )
     }
 }
