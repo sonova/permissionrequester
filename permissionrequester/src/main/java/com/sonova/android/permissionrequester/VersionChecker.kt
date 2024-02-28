c6738cfd449972859f199c7bc644e2abfa444cd4
Carla Vaquero <carla.vaqueroverde@sonova.com>
2024-02-28 08:38:57 +0100

    adds comment to explain functionality

--- permissionrequester/src/main/java/com/sonova/android/permissionrequester/VersionChecker.kt
+++ permissionrequester/src/main/java/com/sonova/android/permissionrequester/VersionChecker.kt
@@ -7,6 +7,13 @@ import androidx.annotation.ChecksSdkIntAtLeast
  * Wrapper to allow overriding android version in testing
  */
 public object VersionChecker {
+
+    /**
+     * Checks that the buildVersion is equal or higher than the sdk of the phone.
+     *
+     * @param buildVersion expected version to check
+     * @return true if phone version >= buildVersion
+     */
     @ChecksSdkIntAtLeast(parameter = 0)
     public fun isBuildVersionUpwards(buildVersion: Int): Boolean =
         Build.VERSION.SDK_INT >= buildVersion
